c41cae88860d17a9dda75f91b093101962418907
Lukas Bystricky <lb13f@my.fsu.edu>
2015-12-27 01:18:33 -0500

    plot_potential : modified code to run on MATLAB R2015a
    Edited code to do transparent contour plot on MATLAB R2015a. Modified paths to resources.
    
    Signed-off-by: Lukas Bystricky <lb13f@my.fsu.edu>

--- Environment/Geopoential/analysis/plot_potential.m
+++ Environment/Geopoential/analysis/plot_potential.m
@@ -1,4 +1,10 @@
-N = 50;
+addpath ../
+addpath ../coefficients
+addpath ../../../Utilities/Math
+
+close all
+
+N = 100;
 az = linspace(0,2*pi,N);
 el = linspace(-pi/2,pi/2,N);
 
@@ -6,20 +12,15 @@ el = linspace(-pi/2,pi/2,N);
 r = 6.371e6 + 200e3*1.6; %200 miles above mean Earth surface
 mu = 3.986004415e14;
 
-% J = [0, 1.7555e25, 0];
-% %J = [0, 0, -2.619e29];
-% S = 0;
-% C = 0;
-
 u = zeros(size(AZ));
 g = zeros(size(AZ));
 
 coeffs_all = dlmread('egm96_to360.ascii');
-coeffs = coeffs_all(2:300,:);
+coeffs = coeffs_all(2:50,:);
 
 for i = 1:length(az)
     for j = 1:length(el)
-        [u(i,j), g_vec] = gravitational_acceleration_harmonics(r, AZ(i,j), EL(i,j), coeffs);
+        [u(i,j), g_vec] = geopotential(r, AZ(i,j), EL(i,j), coeffs);
         
         g(i,j) = norm(g_vec);        
     end
@@ -28,15 +29,16 @@ end
 figure();
 [X, Y, Z] = sph2cart(AZ, EL, r);
 surf(X, Y, Z, u, 'EdgeColor', 'none');
+title('Gravitational Potential');
 colorbar;
 shading interp
 
 figure;
 surf(X, Y, Z, g - mu/r^2, 'EdgeColor', 'none');
+title('Gravitational Acceleration');
 colorbar;
 shading interp
 
-opengl software;
 figure;
 
 gtmp1 = g(:,1:N/2);
@@ -48,20 +50,15 @@ img = imread('coast.jpg');
 imagesc([-180,180],[-90, 90], img);
 hold all;
 
-[C,h] = contourf(az*(180/pi)-180, el*(180/pi), flipud(gfinal- mu/r^2), 30, 'EdgeColor', 'none');
+[~,h] = contourf(az*(180/pi)-180, el*(180/pi), flipud(gfinal- mu/r^2), 30, 'EdgeColor', 'none');
 set(gca, 'clim',[min(min(gfinal- mu/r^2)), max(max(gfinal- mu/r^2))]);
-%set(gcf, 'Renderer', 'OpenGL');
+
+colormap(jet)
 colorbar
 
-ph = findobj(h, '-property', 'FaceAlpha');
-set(ph, 'FaceAlpha', 0.05);
-% for k = [1:-0.1:0.1 0.1:0.1:1]
-%  set(ph, 'FaceAlpha', k);
-%  pause(0.5)
-% end
-
-
-
-
-
-
+pause(0.05)
+hFaces = h.FacePrims;
+for faceIdx = 1 : numel(hFaces)
+   hFaces(faceIdx).ColorType = 'truecoloralpha';  % default = 'truecolor'
+   hFaces(faceIdx).ColorData(4) = 150;   % default=255
+end
