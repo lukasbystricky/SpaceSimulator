137dc8a28eeb61f4f632ed69f1129e117588efb5
Lukas Bystricky <lb13f@my.fsu.edu>
2016-01-07 22:25:24 -0500

    geopotential.m : added comments, fixed bug
    Added comments. Fixed bug involving calculation of the potential. Tested with
    plot_undulation.m. Seems to work fine; no further changes expected.
    
    Signed-off-by: Lukas Bystricky <lb13f@my.fsu.edu>

--- Environment/Geopoential/geopotential.m
+++ Environment/Geopoential/geopotential.m
@@ -1,7 +1,33 @@
 function [u, g] = geopotential(r, az, el, coeffs)
 
-MU = 3.986004415e14;
-A = 6.3781363e6;
+%% geopotential Spherical harmonic expansion to calculate geopotential and
+% accleration
+% [u, g] = geopotential(r, az, el, coeffs) implements a spherical harmonic
+% expansion to calculate the gravitational potential of a geoid. The
+% acceleration is also calculated.
+%
+% Given: 
+% - a radius,r 
+% - an azimuth angle, az
+% - an elevation angle, el
+% - a set of harmonic coefficients, coeffs
+% this model returns:
+% - the gravitational potential, u
+% - the gravitational accleration, g
+%
+% The radius is given in meters and the geocentric elevation and azimuth 
+% angles are given in radians. The coefficients must be fully normalized
+% and given in a Nx6 matrix, with the first 4 columns being in order: n, m,
+% Cnm, Snm. 
+%
+% The accleration returned is a 3x1 vector. This vector is first calculated
+% as the gradient of the potential in spherical coordinates. It is then
+% transformed to cartesian coordinates, relative to the prime meridian.
+% This method of calculating the acceleration results in singularities at
+% the poles. 
+
+MU = 3.986004415e14;      % Gravitational parameter of Earth    [km^3 s^-2]
+A = 6.3781363e6;          % Mean equitorial radius              [m]
 
 u    = -MU/r;
 g    = zeros(3,1);
@@ -23,7 +49,7 @@ if (ncols > 1)
         Pnm = P(m+1,n+1);
         Pnmd = Pd(m+1,n+1);
         
-        u = u - A^n*MU/r*(Pnm*(C*cos(m*az) + S*sin(m*az)))/(r^(n+1));
+        u = u - A^n*MU*(Pnm*(C*cos(m*az) + S*sin(m*az)))/(r^(n+1));
         
         g(1) = g(1) + A^n*(n+1)*MU*Pnm*(C*cos(m*az) + S*sin(m*az))/(r^(n+2));
         g(2) = g(2) - A^n*MU*Pnmd*cos(el)*(C*cos(m*az) + S*sin(m*az))/(r^(n+2));
